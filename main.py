3f2d9d71fa6488ba9f72ec97bba12dc8d273095e
joer0k <ryabokon.niki08@gmail.com>
2025-03-12 15:52:57 +0300

    задача 'цвет каюты'

--- main.py
+++ main.py
@@ -92,5 +92,10 @@ def distribution():
     return render_template('distribution.html', data=names)
 
 
+@app.route('/table/<sex>/<age>')
+def table(sex, age):
+    return render_template('table.html', sex=sex, age=int(age))
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
